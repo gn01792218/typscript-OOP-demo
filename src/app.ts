ef00b168dcad3778efbe5236e6a09cd2d18663ed
gn01792218 <gn01792218@hotmail.com>
2022-07-11 15:33:16 +0800

    修改class的宣告檔與導入方式

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,6 @@
 // Code
-// import {RenderHTML} from './class/RenderHTML'
-class RenderHTML {
-    element:HTMLElement
-    //創建時，輸入template並選擇要掛載到哪個host上
-    constructor(private template:HTMLTemplateElement,private hostEle:HTMLDivElement){
-        //將template的東西導入,true表示深度拷貝
-        const templateNode = document.importNode(this.template.content,true)
-        this.element = templateNode.firstElementChild as HTMLElement
-        //掛載到hostEle上
-        this.hostEle.insertAdjacentElement('afterbegin',this.element)
-    }
-}
+import {RenderHTML} from './class/class'
+
 
 //渲染form元素
 const formTemplate = document.getElementById('form') as HTMLTemplateElement
